aa5ed80f3fbc9ffca35d7438dc91932efcfd3bb4
dakson99 <darko99.mikavica@gmail.com>
2024-08-14 16:48:08 +0200

    checking if the taskArray is empty

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ labels.forEach((label) => {
         span.innerHTML = input.value;
     });
 });
+
+let tasksArr = [
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        date: "14 Avg 2024",
+        time: "16:10",
+    },
+    {
+        title: "Task 2",
+        description: "Task 2 description",
+        date: "14 Avg 2024",
+        time: "16:10",
+    },
+    {
+        title: "Task 3",
+        description: "Task 3 description",
+        date: "14 Avg 2024",
+        time: "16:10",
+    },
+    {
+        title: "Task 4",
+        description: "Task 4 description",
+        date: "14 Avg 2024",
+        time: "16:10",
+    },
+]
+
+const tasksWrapper = document.querySelector(".task-wrapper");
+
+function renderTasks() {
+    tasksWrapper.innerHTML = "";
+
+    //taskArray empty
+    if (tasksArr.length === 0) {
+        tasksWrapper.innerHTML = `<div class="no-tasks">No tasks, Add one now</div>`;
+        return;
+    }
+}
+
+renderTasks();
+
